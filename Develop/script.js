c52c42a477b48c7f3626e7fe21659cc13b1228cf
Artemis <isaac.thorell@outlook.com>
2022-09-25 12:21:18 -0600

    final project

--- Develop/script.js
+++ Develop/script.js
@@ -22,17 +22,48 @@ var dateEl = $("#currentDay")
 dateEl.text(moment().format('dddd, MMMM Do, YYYY'))
 
 
-var saveBtn = $(".saveBtn")
+var saveBtn = $(".saveBtn");
 
-saveBtn.click(function(event){
-    console.log(this);
-    var timeblock = $(".time-block");
-    var plan = $("textarea");
+saveBtn.click(function (event) {
 
-   console.log($(this).siblings("textarea").val());
+    const time = $(this).siblings("textarea").val();
+    const saveInfo = $(this).parent().attr('id');
+    // console.log(saveInfo);
+    // console.log(time);
 
-   localStorage.setItem(JSON.parse$(this).siblings("textarea").val())
+    localStorage.setItem(saveInfo, time);
+    // console.log(localStorage);
+});
+
+function colors() {
+    var currentTime = moment().hours();
+    // console.log(currentTime);
+
+    $('.time-block').each(function () {
+        var blockHour = parseInt($(this).attr('id'));
+        // console.log(blockHour);
+
+        if (blockHour < currentTime) {
+            $(this).addClass('past');
+        };
+        if (blockHour == currentTime) {
+            $(this).addClass('present');
+        };
+        if (blockHour > currentTime) {
+            $(this).addClass('future')
+        }
+
+    })
+};
 
 
-
-})
+$('#9 .plan').val(localStorage.getItem('9'));
+$('#10 .plan').val(localStorage.getItem('10'));
+$('#11 .plan').val(localStorage.getItem('11'));
+$('#12 .plan').val(localStorage.getItem('12'));
+$('#13 .plan').val(localStorage.getItem('13'));
+$('#14 .plan').val(localStorage.getItem('14'));
+$('#15 .plan').val(localStorage.getItem('15'));
+$('#16 .plan').val(localStorage.getItem('16'));
+$('#17 .plan').val(localStorage.getItem('17'));
+colors()
